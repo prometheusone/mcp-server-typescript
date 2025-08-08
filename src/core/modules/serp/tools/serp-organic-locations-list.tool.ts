bb1d7132fe9df88e1119c9850096b2adf137238c
lVics <thewalkingundeads@gmail.com>
2025-08-08 11:27:18 +0300

    fix remarks

--- src/core/modules/serp/tools/serp-organic-locations-list.tool.ts
+++ src/core/modules/serp/tools/serp-organic-locations-list.tool.ts
@@ -13,7 +13,7 @@ export class SerpOrganicLocationsListTool extends BaseTool {
   }
 
   getDescription(): string {
-    return 'Utility tool for serp_organic_live_advanced to get list of availible locations. At least 1 payload field must be specified';
+    return 'Utility tool for serp_organic_live_advanced to get list of availible locations.';
   }
 
   protected supportOnlyFullResponse(): boolean {
@@ -23,7 +23,7 @@ export class SerpOrganicLocationsListTool extends BaseTool {
   getParams(): z.ZodRawShape {
     return {
       search_engine: z.string().default('google').describe("search engine name, one of: google, yahoo, bing."),
-      country_iso_code: z.string().optional().describe("ISO 3166-1 alpha-2 country code, for example: US, GB, MT"),
+      country_iso_code: z.string().describe("ISO 3166-1 alpha-2 country code, for example: US, GB, MT"),
       location_type: z.string().optional().describe("Type of location. Possible variants: 'TV Region','Postal Code','Neighborhood','Governorate','National Park','Quarter','Canton','Airport','Okrug','Prefecture','City','Country','Province','Barrio','Sub-District','Congressional District','Municipality District','district','DMA Region','Union Territory','Territory','Colloquial Area','Autonomous Community','Borough','County','State','District','City Region','Commune','Region','Department','Division','Sub-Ward','Municipality','University'"),
       location_name: z.string().optional().describe("Name of location or it`s part.")
     };
@@ -32,20 +32,16 @@ export class SerpOrganicLocationsListTool extends BaseTool {
   async handle(params:any): Promise<any> {
     try {
 
-      const payload: Record<string, unknown> = {};
+      const payload: Record<string, unknown> = {
+        'country_iso_code': params.country_iso_code,
+      };
 
-      if (params.country_iso_code) {
-        payload['country_iso_code'] = params.country_iso_code;
-      }
       if (params.location_type) {
         payload['location_type'] = params.location_type;
       }
+      
       if (params.location_name) {
         payload['location_name'] = params.location_name;
-      }
-
-      if (Object.keys(payload).length === 0) {
-        throw new Error('At least one of the following fields must be specified: country_iso_code, location_type, location_name');
       }
 
       const response = await this.dataForSEOClient.makeRequest(`/v3/serp/${params.search_engine}/locations`, 'POST', [payload]) as DataForSEOResponse;
